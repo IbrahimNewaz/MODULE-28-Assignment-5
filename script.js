4b418f8f40bdc778c55969c4e3e47468f6fe8ca0
IbrahimNewaz <ibrahim.codecoffe@gmail.com>
2025-08-29 17:58:32 +0600

    All Functionalities Done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// 🟩🟦 Nav bar like hart counting Functionalities 🟦🟩 //
+
 const hartIcons = document.getElementsByClassName('hartClick');
 const likeCounter = document.getElementById('like-count');
 
@@ -13,3 +15,85 @@ for (const icon of hartIcons) {
     });
 }
 
+// 🟩🟦 Nav bar Hotline Call Coin Charge counting Functionalities 🟦🟩 //
+
+// 🚩 this below code is ok // 
+
+const emrCallButtons = document.getElementsByClassName('call-Button');
+const coinCountDisplay = document.getElementById('coin-Count');
+let totalcoinCount = 100;
+
+for (const button of emrCallButtons) {
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        if (totalcoinCount < 20) {
+            alert("You don't have enough coins to make a call.");
+            return;
+        }
+
+        // Get the parent card //
+        const card = button.closest('.EmergencyNumber');
+
+
+        const serviceName = card.querySelector('.card-ttl h3')?.innerText || "Unknown Service";
+        const serviceNumber = card.querySelector('.hotline-number h3')?.innerText || "N/A";
+
+        alert("📞 Calling " + serviceName + " " + serviceNumber + "...");
+
+        totalcoinCount -= 20;
+        coinCountDisplay.innerText = totalcoinCount;
+
+    });
+}
+
+
+// 🟩🟦 Copy part 🟦🟩 //
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     const buttons = document.querySelectorAll(".copy-Button");
+//     for (const button of buttons) {
+//         button.addEventListener("click", function () {
+//             const card = button.closest(".EmergencyNumber");
+
+//             const textElement = card.querySelector(".hotline-number h3");
+
+//             const textToCopy = textElement.innerText;
+
+//             navigator.clipboard.writeText(textToCopy)
+
+//                 .then(function () {
+//                     alert("The number has been copied. " + textElement.innerText);
+//                 })
+//                 .catch(function () {
+//                     alert("Error Cant Copy");
+//                 });
+//         });
+//     }
+// });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".copy-Button");
+    const navbarCopyCounter = document.getElementById("copy-count");
+    let copyCount = 0;
+
+    for (const button of buttons) {
+        button.addEventListener("click", function () {
+            const card = button.closest(".EmergencyNumber");
+            const textElement = card.querySelector(".hotline-number h3");
+            const textToCopy = textElement.innerText;
+
+            navigator.clipboard.writeText(textToCopy)
+                .then(function () {
+                    alert("The number has been copied. " + textElement.innerText);
+                    copyCount++;
+                    navbarCopyCounter.innerText = copyCount;
+                })
+
+                .catch(function () {
+                    alert("Error Cant Copy");
+                });
+        });
+    }
+});
